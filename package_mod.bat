69b7e080ac25dc883fe64985ac95941f890c43c0
Mike <mike1072@gmail.com>
2015-12-31 15:02:23 -0800

    Hotfix 17: fix wakizashi/ninja-to patching

--- package_mod.bat
+++ package_mod.bat
@@ -6,7 +6,7 @@ call "%~dp0..\ModPackaging\utilities\ie_games.bat"
 
 REM /* MODIFY: set the values of the 3 variables below to reflect the current mod version */
 set "mod_name=Item Revisions"
-set mod_version=v3.0.16
+set mod_version=v3.0.17
 set mod_folder=item_rev
 
 REM /* MODIFY: list here which IE games the mod is compatible with, from this list of possibilities: */
